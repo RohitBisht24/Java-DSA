75e6b644d3a98137b6c6049a898d175be523a055
Rohit Singh Bisht <rohitsinghbisht568@gmail.com>
2025-08-22 23:35:01 +0530

    fix small syntax error in math function

--- mathFunctions.java
+++ mathFunctions.java
@@ -2,7 +2,7 @@ public class mathFunctions {
     public static void main(String[] args) {
         System.out.println(Math.min(2, 5));
         System.out.println(Math.max(22, 75));
-        System.out.println((int) (12.0));
+        System.out.println((int) Math.floor(12.0));
         System.out.println((int) Math.ceil(12.1));
         System.out.println((int) Math.pow(2, 3));
         System.out.println(Math.sqrt(81));
