2a048c52e9f3bdc1065edd1dcb3ab394ee6af8f0
Rohit Singh Bisht <rohitsinghbisht568@gmail.com>
2025-08-26 13:32:22 +0530

    oneline remove

--- Array/printSubarrays.java
+++ Array/printSubarrays.java
@@ -3,8 +3,6 @@ package Array;
 public class printSubarrays {
 
     public static void subArrays(int arr[]) {
-
-        int TS = 0;
         for (int i = 0; i < arr.length; i++) {
             int start = i;
             for (int j = i; j < arr.length; j++) {
@@ -12,7 +10,7 @@ public class printSubarrays {
                 for (int k = start; k <= end; k++) {
                     System.out.print(arr[k] + " ");
                 }
-                System.out.println(); 
+                System.out.println();
             }
             System.out.println();
         }
